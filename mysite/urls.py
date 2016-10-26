a6489a9add75f5494a4acf3e6bcd27f95d0368c8
wufd <wufdong@gmail.com>
2016-10-26 10:48:55 +0800

    默认host

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import include, url
 from django.conf.urls import patterns
 from django.contrib import admin
 
-urlpatterns = patterns('', 
-		url(r'^admin/', include(admin.site.urls)),
-		url(r'^blogs/', include('blogs.urls')),
-		# url(r'^', include('blogs.urls'))
-	) 
+urlpatterns = patterns('',
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^blogs/', include('blogs.urls')),
+                       url(r'^', include('blogs.urls'))
+                       )
